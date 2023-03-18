1356ec980d2a2f612073f1a5aa2998952e24f700
Dio <a0981551262@gmail.com>
2023-03-19 00:00:39 +0800

    message separate (url and text message) by using model re

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ from discord.ext import commands
 import json
 import discord
 import asyncio
+import re
+
+
+#tell between the url and pure text message and seperate them
+def separattion(x):
+    url_test_sample = re.compile(r'http[s]?://\S+')
+    url_match = url_test_sample.search(x)
+    if url_match:
+        url = url_match.group(0)
+        text = x.replace(url, '').strip()
+    else:
+        url = None
+        text = x
+    file_writing(url, text)
+
+def file_writing(input_url, input_text):
+    #print(f"{input_url} {input_text}")
+    with open("message.txt", "w") as message_file:
+        message_file.write(input_url+input_text)
 
 
 #intents setting
@@ -18,12 +37,15 @@ async def on_ready():
     print("Bot is ready")
 
 
+
 @bot.event
 async def on_message(message):
     if message.channel == bot.get_channel(1086095100792279073):
-        with open("message.txt","w") as message_file:
-            message_file.write(message.content)
+        separattion(message.content)
+
+
 #        print(message.content)
+
 
 bot.run(data['token'])
 
